0003ea13868a1444e0ab4a549d63fc465940252a
beto <hello@96levels.com>
2016-01-16 02:02:02 +0100

    Fixes

--- robin.php
+++ robin.php
@@ -1,18 +1,19 @@
 <?
 set_time_limit(0);
-echo PHP_EOL;
-echo '============================================================'.PHP_EOL;
-echo 'ROBIN            '.PHP_EOL;
+date_default_timezone_set('Europe/Madrid'); 
+
+echo PHP_EOL.PHP_EOL;
+echo '==---------------------------------------------------------------=='.PHP_EOL;
+echo '~ ROBIN ~            '.PHP_EOL;
 echo 'Crawling & Scraping Toolkit   '.PHP_EOL;  
 echo '@author: betolopezayesa@gmail.com / @betoayesa'.PHP_EOL;
 echo '@version: 1.0 15/1/2016'.PHP_EOL;
 echo 'The MIT License (MIT)'.PHP_EOL;
 echo 'Contribute https://github.com/natzar/robin'.PHP_EOL;
-echo 'Copyright (c) 2016 Humberto López (betolopezayesa@gmail.com)'.PHP_EOL.PHP_EOL;
-echo '============================================================'.PHP_EOL;
-echo 'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'.PHP_EOL;
-echo PHP_EOL.PHP_EOL;
-
+echo 'Copyright (c) 2016 Humberto López (betolopezayesa@gmail.com)'.PHP_EOL;
+echo 'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND'.PHP_EOL;
+echo '==---------------------------------------------------------------=='.PHP_EOL;
+echo PHP_EOL;
 
 include_once "vendor/uagent.php";
 include_once "vendor/PHPCrawl_081/classes/phpcrawler.class.php";
@@ -22,6 +23,7 @@ include_once "robots/Robot.php";
 // Load all Robots classes
 $dir = opendir(dirname(__FILE__)."/robots");
 $robotsList = array();
+$errors = array();
 while ($current = readdir($dir)){
     if( $current != "." && $current != "..") {
         if (is_dir(dirname(__FILE__)."/robots/".$current)){
@@ -29,24 +31,25 @@ while ($current = readdir($dir)){
                 $robotsList[] = $current;
                 include_once dirname(__FILE__)."/robots/".$current."/".$current.".php";
             }else{
-                echo $current.' not installed properly'.PHP_EOL;
+                $errors[] = $current;
             }
         }
-    }
+    } 
 }
-echo "Installed Robots: ".implode(",",$robotsList).PHP_EOL;
-
+echo "[i] Installed Robots: ".implode(", ",$robotsList).".".PHP_EOL;
+echo "[i] Errors found: ".implode(", ",$errors).".".PHP_EOL;
 
 if (!class_exists($argv[1])){
-    echo 'There is no Robot for '.$argv[1].' at /robots/'.PHP_EOL;
-    echo 'Contribute!';
+    //echo 'There is no Robot for '.$argv[1].' at /robots/'.PHP_EOL;
+   //echo "Installed Robots: ".implode(",",$robotsList).PHP_EOL;
+    echo PHP_EOL.'[i] Usage: php robin.php {Robot} {command} {keyword}'.PHP_EOL;
+    echo '[i] Type just php robin.php {Robot} for help'.PHP_EOL;
     die();
 }
 
 $robot = new $argv[1]($argv);
 
-if (!isset($argv[2]) or !fe( $robot->$argv[2])){
-    echo 'The Robot '.$argv[1].' doesn\'t recognize the command '.$argv[2].PHP_EOL;
+if (!isset($argv[2]) or !is_callable(array($argv[1],$argv[2]))){   
     echo 'Available commands:'.PHP_EOL;
     echo '------------------------------'.PHP_EOL;
     $metodos_clase = get_class_methods($robot);
@@ -56,6 +59,9 @@ if (!isset($argv[2]) or !fe( $robot->$argv[2])){
         echo "[+] $nombre_metodo".PHP_EOL;
     }
   echo '------------------------------'.PHP_EOL;
+    echo PHP_EOL.'[i] Usage: php robin.php {Robot} {command} {keyword}'.PHP_EOL;
+    echo '[i] Type just php robin.php {Robot} for help'.PHP_EOL;
+
   die();
 }
 
@@ -64,5 +70,5 @@ if (!isset($argv[2]) or !fe( $robot->$argv[2])){
 $robot-> $argv[2]();
 
 
-echo '============================================================'.PHP_EOL;
+echo '------------------------------------------------------------'.PHP_EOL;
 
--- robots/Robot.php
+++ robots/Robot.php
@@ -3,6 +3,9 @@ class Robot {
     var $args;
     function __construct($argv){
         $this->args = $argv;
-        echo '[I] New Robot trying job '.$argv[2].' on '.$argv[1].PHP_EOL;
+        echo '==---------------------------------------------------------------=='.PHP_EOL;
+        echo PHP_EOL.'[R] '.$argv[2]."@".strtoupper($argv[1]).PHP_EOL;
+        echo '==---------------------------------------------------------------=='.PHP_EOL;
+        
     }
 }
