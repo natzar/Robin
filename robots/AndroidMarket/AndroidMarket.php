9f67030f480dc1b5fdc2688e1097b993711a1e55
beto <hello@96levels.com>
2016-01-16 12:52:56 +0100

    AndroidMarket more fixes

---
+++ robots/AndroidMarket/AndroidMarket.php
@@ -0,0 +1,10 @@
+<?
+include dirname(__FILE__)."/playStoreApi.php";
+
+class AndroidMarket extends Robot{
+
+    function apps(){
+        die ("[Error] AndroidMarket needs Proxies".PHP_EOL)
+    }
+
+}
