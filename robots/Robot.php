2a8914a9fd82efe3d0a5d1adcd1eb4a6038fa330
beto <hello@96levels.com>
2016-01-17 01:24:23 +0100

    json results

--- robots/Robot.php
+++ robots/Robot.php
@@ -3,6 +3,7 @@ class Robot {
     var $args;
     var $keyword;
     var $name;
+    var $results; 
     
     function __construct($argv){
         if (!isset($argv[2])){
@@ -11,19 +12,20 @@ class Robot {
         $this->args = $argv;
         $this->keyword=  @$argv[3];
         $this->name = $argv[2];
+        $this->results = array();
         
         echo '[Selected Robot] '.$argv[2]." @ ".strtoupper($argv[1]).PHP_EOL;
         echo '==---------------------------------------------------------------=='.PHP_EOL;
     }
     
-    function results(){
-    
+    private function results(){
+        // JSON RESULTS in file
+        $d = Date("d/m-h:i:s");
+        $file = fopen(dirname(__FILE__)."/../downloads/".$argv[1]."-".$argv[2]."-".$d.".json","w");
+        fwrite($file,json_encode($this->results))    
+        fclose($file);
     }
     
-    function jsonResults($r){    
-        echo json_encode($r);    
-    }
-    function txtResults(){
-    }
+    
 
 }
